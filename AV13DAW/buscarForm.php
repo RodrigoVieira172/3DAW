5304de8b337f252695aa38a9ff7fe5ce97d2062e
RodrigoVieira <rodrigo.1720470400003@faeterj-rio.edu.br>
2020-10-20 16:02:28 -0300

    Atualizada o nome da pasta da prova AV1 de 3DAW

--- AV13DAW/buscarForm.php
+++
@@ -1,6 +0,0 @@
-<form action="buscarServer.php" method="POST">
-<h4>Digite o número de matrícula do aluno desejado:</h4>
-<input type="text" class="form-control" name="matricula" placeholder="Matrícula" /> <br>
-
-<input type="submit" class="btn btn-dark" value="Buscar" />
-</form>
